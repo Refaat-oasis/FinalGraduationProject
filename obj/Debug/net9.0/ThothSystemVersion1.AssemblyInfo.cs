ce25f4c92ec3a563efbae1d8fceff3f33b55166b
Refaat-oasis <ahta601@gmail.com>
2025-02-18 13:22:39 +0200

    some modifications

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa17af7385af4cfc75261b82c45234253e8de41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a44e995ab75d8ce2894e6f693628f66aa1233f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

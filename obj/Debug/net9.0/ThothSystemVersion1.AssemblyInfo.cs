f2406003c042968683e7a9d415ecc57a6c8ec9c2
Refaat-oasis <ahta601@gmail.com>
2025-02-21 18:08:29 +0200

    modifactions

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c52d18b446b9288a0a843e8d2cc47cf70d66ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe4f02bf33809b9ca71dcc3a14dadbebfc57ee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

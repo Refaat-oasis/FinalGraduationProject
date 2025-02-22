0a557627370c1973702c53a8ef5a070ef212914c
Refaat-oasis <ahta601@gmail.com>
2025-02-22 14:57:12 +0200

    modification to the view all and adding the utilities

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2406003c042968683e7a9d415ecc57a6c8ec9c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fae1b9f0bb3e6c06cdf8c0f6b372bd0b1f1604a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

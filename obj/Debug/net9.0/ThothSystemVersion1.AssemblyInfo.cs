82418daecdb933d29dee39c9af999301cd2f8dc0
Refaat-oasis <ahta601@gmail.com>
2025-03-01 00:29:12 +0200

    the css and the java script are outside the view

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+056d1f8b011f0223acbe4fb9a5fdd7874e2da806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f6e6f6fd905bb971c402f7ba5f5963828a10df")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/loginEmployee.js
@@ -0,0 +1,47 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const myform = document.getElementById("myform");
+    const userName = document.getElementById("userName");
+    const Password = document.getElementById("Password");
+
+    myform.addEventListener("submit", function (e) {
+        if (!validate()) {
+            e.preventDefault();
+        }
+    });
+
+    function validate() {
+        let valid = true;
+
+        if (userName.value.trim() === "") {
+            setError(userName, "برجاء إدخال اسم المستخدم");
+            valid = false;
+        } else {
+            setSuccess(userName);
+        }
+
+        if (Password.value.trim() === "") {
+            setError(Password, "برجاء إدخال كلمة المرور");
+            valid = false;
+        } else {
+            setSuccess(Password);
+        }
+
+        return valid;
+    }
+
+    const setError = (input, errorMsg) => {
+        const inputBox = input.parentElement;
+        const errorParagraph = inputBox.querySelector(".error");
+        errorParagraph.innerText = errorMsg;
+        inputBox.classList.remove("success");
+        inputBox.classList.add("error");
+    };
+
+    const setSuccess = (input) => {
+        const inputBox = input.parentElement;
+        const errorParagraph = inputBox.querySelector(".error");
+        errorParagraph.innerText = "";
+        inputBox.classList.remove("error");
+        inputBox.classList.add("success");
+    };
+});

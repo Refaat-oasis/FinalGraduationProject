19ba74cfe2b373deb32f1860df0dab0d277a3946
Refaat-oasis <ahta601@gmail.com>
2025-03-03 12:29:14 +0200

    the add vendor error

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c9e4e1f48d1c4db8929001a8cc243dd4893505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98fed9dcdd01621452b83805c18859aee9645036")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

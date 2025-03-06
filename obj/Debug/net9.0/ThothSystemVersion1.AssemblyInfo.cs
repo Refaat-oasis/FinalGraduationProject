7b1eadee406a126c0daf4dd944fe96b793e179b2
Mariam Bahaa <mariambahaaaa226@gmail.com>
2025-03-06 10:07:47 +0200

    the controllers update

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f04af01b2c58848395f6ab4c3ebf7edd167650")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad815fcecb79a7dd2945d12764b36f467d7ecc5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

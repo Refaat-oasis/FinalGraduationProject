9ac0de15400c6f03912fff59ebeb9a6ee0d42238
Shahd Mamdouh <shoshomamd@gmail.com>
2025-03-08 16:25:03 +0200

    views of ink,paper and supply

---
+++ wwwroot/js/Inventory/ViewAllSupply.js
@@ -0,0 +1,24 @@
+﻿function searchSupply() {
+    var input, filter, table, tr, td, i, j, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("SupplyTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows (skip the header row)
+    for (i = 1; i < tr.length; i++) {
+        tr[i].style.display = "none"; // Hide the row by default
+        td = tr[i].getElementsByTagName("td");
+
+        // Loop through all columns in the current row
+        for (j = 0; j < td.length; j++) {
+            if (td[j]) {
+                txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = ""; // Show the row if a match is found
+                    break; // Exit the inner loop if a match is found
+                }
+            }
+        }
+    }
+}

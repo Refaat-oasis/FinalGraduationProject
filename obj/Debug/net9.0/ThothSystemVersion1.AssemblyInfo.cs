417940e327681a058998d737d4ebcb00e0e55b89
Refaat-oasis <ahta601@gmail.com>
2025-03-29 15:01:00 +0200

    physical count order view made by sandra

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd2c565fb233d62ba929c67ddc4bb0d5d9fbcf11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dcb6535f3ade0eeb7545c14a514629885adc763")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

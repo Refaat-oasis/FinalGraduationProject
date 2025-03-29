67610d9435798f7861327528f4db1d36a3eafc0d
mira hany <mirahanysamue@gmail.com>
2025-03-29 12:16:05 +0200

    this is the purchase view of ink,supply,paper

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6d2ef8f7eb367fbb0bbbe7bdbbd34368c990a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd2c565fb233d62ba929c67ddc4bb0d5d9fbcf11")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

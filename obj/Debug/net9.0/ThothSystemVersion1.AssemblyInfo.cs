18178011d8e3e10260f75b60da8209b89c6812ce
Refaat-oasis <ahta601@gmail.com>
2025-04-03 21:57:27 +0200

    the signalR now works in the admin home and removed it from the view of paper , ink , supply

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaef3b621b151e6b9f214746b5137d037b4d6b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c4a2d520070e7bee44249a3e52ab552a42f2e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

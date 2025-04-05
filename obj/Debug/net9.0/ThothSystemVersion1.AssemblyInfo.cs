7ac87885acba8d87873d420b3122c7aee785784a
KhadijaAlashry <khokhaashry969@gmail.com>
2025-04-05 23:15:28 +0200

    front of requisite order

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6a4f048d7b4cb6edf7838d977af29e641cc0283")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b21e19335e3b7884aaa09037a84ec116090da9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

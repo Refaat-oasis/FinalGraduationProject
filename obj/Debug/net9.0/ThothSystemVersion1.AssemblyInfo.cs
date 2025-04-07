d4940b809a8558d64326a2a5db73440556d304a4
Shahd Mamdouh <shoshomamd@gmail.com>
2025-04-08 01:16:07 +0200

    edits everywhere

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59213901d285903f3960f8ec8536afd9d474d37b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f7427277ca84b3e3755f7321a1d4184ad1a524")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

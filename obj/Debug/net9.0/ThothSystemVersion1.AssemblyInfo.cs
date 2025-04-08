0e3d8f031dc10862a2b1b778675ece1330ead9d1
Refaat-oasis <ahta601@gmail.com>
2025-04-08 22:18:54 +0200

    find the return problem

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e86407993ec35285b23e639ece65625d29cac04c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc28905cf2c8956353d6f5fda6216ce6cd6a2d7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

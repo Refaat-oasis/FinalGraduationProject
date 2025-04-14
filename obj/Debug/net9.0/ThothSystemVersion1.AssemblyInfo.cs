34112c0866b1b932c137a43f1f4142f3428a7bdf
Shahd Mamdouh <shoshomamd@gmail.com>
2025-04-14 20:13:21 +0200

    edit redirect and reorder msg

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6366a15b93c57aa4d33537c7f5decbc5d57fc5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdbd27a5a51df204e293293dedc66bd9c74ba4a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

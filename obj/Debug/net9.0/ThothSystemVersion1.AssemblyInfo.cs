628154c28bbb5f7d679b5beb4aef4e99eb5a0c28
Shahd Mamdouh <shoshomamd@gmail.com>
2025-04-18 17:09:49 +0200

    edit views view machine and labour

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364419f9e9a1fccc1e7b84ffabc50c04c809a4b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e801e091c8db4d6cdcf6ac8e28ff6f4a436125")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

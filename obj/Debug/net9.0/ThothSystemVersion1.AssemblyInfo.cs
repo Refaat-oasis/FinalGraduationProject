f6e9578c304ae1cf0239da2ed9247a94f76dedec
refaat-oasis <ahta601@gmail.com>
2025-06-16 01:44:30 +0300

    purchase now in ink depend on the average quantity in the ink table

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+808701c0fe43ecf607117c1472e8f39d62d829ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094705cc57441d161886d0762fd0b7f0f1606102")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

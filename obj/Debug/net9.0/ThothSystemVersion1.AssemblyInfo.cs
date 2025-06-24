ab85a47ca38c193221d33db01f8273796fd38351
mira hany <mirahanysamue@gmail.com>
2025-06-25 01:13:04 +0300

    changing the homes of all users

--- obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ThothSystemVersion1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0c517f80d8fad0d0aca4df88a427a4ea3e6eb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b5ab6f895ad2c77fdc1765fd61f4805d4f2c3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThothSystemVersion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
